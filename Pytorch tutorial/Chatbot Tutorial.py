8d67e88d321094aec35cc730653d189661cdf649
POI <1309416226@qq.com>
2019-09-13 22:03:03 +0800

    9.13
    总算把这个教程全敲完了，现在慢慢琢磨吧

--- Pytorch tutorial/Chatbot Tutorial.py
+++ Pytorch tutorial/Chatbot Tutorial.py
@@ -673,11 +673,234 @@ class LuongAttnDecoderRNN(nn.Module):
         #Perform backpropatation
         loss.backward()
         
+        #Clip gradients:gradients are modified in place
+        _=nn.utils.clip_grad_norm_(encoder.parameters(),clip)
+        _=nn.utils.clip_grad_norm_(decoder.parameters(),clip)
+        
+        #Adjust model weights
+        encoder_optimizer.step()
+        decoder_optimizer.step()
+        
+        return sum(print_loss)/n_totals
+    
+#Training iterations
+        
+def trainIters(model_name,voc,pairs,encoder,decoder,encoder_optimizer,decoder_optimizer,
+               embedding, encoder_n_layers,decoder_n_layers,save_dir,n_iteration,batch_size,print_every,
+               save_every,clip,corpus_name,loadFilename):
+    
+    #Load batchers for each iteration
+    training_batches=[batch2TrainData(voc,[random.choice(pairs) for _ in range(batch_size)])
+    for _ in range(n_iteration)]
+    
+    #Initialization
+    print("Initializing...")
+    start_iteration=1
+    print_loss=0
+    if loadFilename:
+        start_iteration=checkpoint['iteration']+1
+        
+    #Training loop
+    print("Training...")
+    for iteration in range(start_iteration,n_iteration+1):
+        training_batch=training_batches[iteration-1]
+        #Extract fields from batch
+        input_variable,lengths,target_variable,mask,max_target_len=training_batch
+        
+        #Run a training iteration with batch
+        loss=train(input_variable,lengths,target_variable,mask,max_target_len,encoder,
+                   decoder,embedding,encoder_optimizer,decoder_optimizer,batch_size,clip)
+        print_loss+=loss
+        
+        #Print progress
+        if iteration%print_every==0:
+            print_loss_avg=print_loss/print_every
+            print("Iteration:{};Percent complete:{:.1f}%;Average loss:{:.4f}".format(iteration,
+                  iteration/n_iteration*100,print_loss_avg))
+            print_loss=0
+            
+        #Save checkpoint
+        if(iteration%save_every==0):
+            direction=os.path.join(save_dir,model_name,corpus_name,'{}-{}_{}'.format(encoder_n_layers,
+                                   decoder_n_layers,hidden_size))
+            if not os.path.exists(diretory):
+                os.makedirs(directory)
+            torch.save({
+                    'iteration':iteration,
+                    'en':encoder.state_dict(),
+                    'de':decoder.state_dict(),
+                    'en_opt':encoder_optimizer.state_dict(),
+                    'de_opt':decoder_optimizer.state_dict(),
+                    'loss':loss,
+                    'voc_dict':voc.__dict__,
+                    'embedding':embedding.state_dict()
+                },os.path.join(directory,'{}_{}.tar'.format(iteration,'checkpoint')))
+    
+
+#Define Evaluation 
+class GreedySearchDecoder(nn.Module):
+    def __init__(self,encoder,decoder):
+        super(GreedySearchDecoder,self).__init__()
+        self.encoder=encoder
+        self.decoder=decoder
+        
+    def forward(self,input_seq,input_length,max_length):
+        #Forward input through encoder model
+        encoder_outputs,encoder_hidden=self.encoder(input_seq,input_length)
+        #Prepare encoder's final hidden layer to be first hidden input to the decoder
+        decoder_hidden=encoder_hidden[:decoder.n_layers]
+        #Initialize decoder input with SOS_token
+        decoder_input=torch.ones(1,1,device=device,dtype=torch.long)*SOS_token
+        #Initialize tensors to append decoded words to
+        all_tokens=torch.zeros([0],device,dtype=torch.long)
+        all_scores=torch.zeros([0],device=device)
+        #Iteratively decode one word token at a time
+        for _ in range(max_length):
+            #Forward pass through decoder
+            decoder_output,decoder_hidden=self.decoder(decoder_input,decoder_hidden,
+                                                       encoder_outputs)
+            #Obtain most likely word token and its softmax score
+            decoer_scores,decoder_input=torch.max(decoder_output,dim=1)
+            #Record token and score
+            all_tokens=torch.cat((all_tokens,decoder_input),dim=0)
+            all_scores=torch.cat((all_scores,decoder_scores),dim=0)
+            #Prepare current token to be next decoder input (add a dimension)
+            decoder_input=torch.unsqueeze(decoder_input,0)
+        #Return collections of word tokens and scores
+        return all_tokens,all_scores
+
+#Evaluate my text
+def evaluate(encoder,decoder,searcher,voc,sentence,max_length=MAX_LENGTH):
+    #Formate input sentence as a batch
+    #words->indexes
+    indexes_batch=[indexesFromSentence(voc,sentence)]
+    #Create lengths tensor
+    lengths=torch.tensor([len(indexes)for indexes in indexes_batch])
+    #Transpose dimension of batch to match model's exceptations
+    input_batch=torch.LongTensor(indexes_batch).transpose(0,1)
+    #Use appropriate device
+    input_batch=input_batch.to(device)
+    lengths=lengths.to(device)
+    #Decode sentence with searcher
+    tokens,scores=searcher(input_batch,lengths,max_length)
+    #indexes->words
+    decoded_words=[voc.index2word[token.item()]for token in tokens]
+    return decoded_words
+
+def evaluateInput(encoder,decoder,searcher,voc):
+    input_sentence=''
+    while(1):
+        try:
+            #Get input sentence
+            input_sentence=input('>')
+            #Check if it is quit case
+            if input_sentence=='q'or input_sentence=='quit':break
+            #Normalize sentence
+            input_sentence=normalizeString(input_sentence)
+            #Evaluate sentence
+            output_words=evaluate(encoder,decoder,searcher,voc,input_sentence)
+            #Format and print response sentence
+            output_words[:]=[x for x in output_words if not (x=='EOS' or x=='PAD')]
+            print('Bot:',' '.join(output_words))
+            
+        except KeyError:
+            print("Error:Encouted unknown word.")
 
 
+#Run model
+
+#Configure models
+model_name='cb_model'
+attn_model='dot'
+#attn_model='general'
+#attn_model='concat'
+hidden_size=500
+encoder_n_layers=2
+decoder_n_layers=2
+dropout=0.1
+batch_size=64
+#Set checkpoint to load from; set to None if starting from scratch
+loadFilename=None
+checkpoint_iter=4000
+#loadFirename=os.path.join(save_dir,model_name,corpus,name,
+#                           '{}-{}_{}'.format(encoder_n_layers,decoder_n_layers,hidden_size),
+#                           '{}_checkpoint.tar'.format(checkpoint_iter))
+
+#Load model if a loadFilename is provided
+if loadFilename:
+    #If loading on same machine the model was trained on
+    checkpoint=torch.load(loadFilename)
+    #If loading a model trained on GPU to   CPU
+    # checkpoint=torch.load(loadFilename,map_location=torch.device('cpu'))
+    encoder_sd=checkpoint['en']
+    decoder_sd=checkpoint['de']
+    encoder_optimizer_sd=checkpoint['en_opt']
+    decoder_optimizer_sd=checkpoint['de_opt']
+    embedding_sd=checkpoint['embedding']
+    voc.__dict__=checkpoint['voc_dict']
+    
+print('Building encoder and decoder...')
+#   Initialize word embeddings
+embedding=nn.Embedding(voc.num_words,hidden_size)
+if loadFilename:
+    embedding.load_state_dict(embedding_sd)
+#Initialize encoder & decoder models
+encoder=EncoderRNN(hidden_size,embedding,encoder_n_layers,dropout)
+decoder=LuongAttnDecoderRNN(attn_model,embedding,hidden_size,voc.num_words,decoder_n_layers,dropout)
+if loadFilename:
+    encoder.load_state_dict(encoder_sd)
+    decoder.load_state_dict(decoder_sd)
+#Use appropriate device
+encoder=encoder.to(device)
+decoder=decoder.to(device)
+print('Models built and ready to go!')
+    
+#Run Training
+
+#Configure training/optimization
+clip=50.0
+teacher_forcing_ratio=1.0
+learning_rate=0.0001
+decoder_learning_ratio=5.0
+n_iteration=4000
+print_every=1
+save_every=500
+
+#Ensure dropout layers are in train mode
+encoder.train()
+decoder.train()
+
+#Initialize optimizers
+print('Building optimizers...')
+encoder_optimizer=optim.Adam(encoder.parameters(),lr=learning_rate)
+decoder_optimizer=optim.Adam(decoder.parameters(),lr=learning_rate*decoder_learning_ratio)
+if loadFilename:
+    encoder_optimizer.load_state_dict(encoder_optimizer_sd)
+    decoder_optimizer.load_state_dict(decoder_optimizer_sd)
+
+#If you have cuda, configure cuda to call
+for state in encoder_optimizer.state.values():
+    for k,v in state.items():
+        if isinstance(v,torch.Tensor):
+            state[k]=v.cuda()
+
+# Run training iterations
+print("Starting Training!")
+trainIters(model_name,voc,pairs,encoder,decoder,encoder_optimizer,decoder_optimizer,
+           embedding,encoder_n_layers,decoder_n_layers,save_dir,n_iteration,batch_size,
+           print_every,save_every,clip,corpus_name,loadFilename)
 
 
+#Run evaluation
+#Set dropout layers to eval mode
+encoder.eval()
+decoder.eval()
 
+#Initialize search module
+searcher=GreedySearchDecoder(encoder,decoder)
+
+#Begin chatting(uncomment and run the following line to begin)
+#evaluateInput(encoer,decoder,searcher,voc)
 
 
 
