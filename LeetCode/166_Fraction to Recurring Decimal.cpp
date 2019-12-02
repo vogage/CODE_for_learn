582cb43129c356bced7f06501238e720e5cb7661
vogage <1309416226@qq.com>
2019-12-03 00:39:28 +0800

    12-3
    最后这个Corner problem有点搞笑，在我的vs上根本运行不起来，但是还是得注意一下，就是abs的运算问题，在INTeger的满位时，正负情况可能会有问题

--- LeetCode/166_Fraction to Recurring Decimal.cpp
+++ LeetCode/166_Fraction to Recurring Decimal.cpp
@@ -1,5 +1,7 @@
 #include<stdio.h>
 #include<string>
+#include<unordered_map>
+#include<algorithm>
 
 using namespace std;
 
@@ -72,7 +74,7 @@ public:
 		for (int i = 0; i < n / 2; i++) {
 			string temp1 = str.substr(n - i - 1, i+1);
 			string temp2 = str.substr(n - i * 2 - 2, i+1);
-			if (temp1 == temp2&&!(temp1=="0"&& temp2=="0")) {
+			if (temp1 == temp2 && !(temp1 == "0"&& temp2 == "0")) {
 				flag = true;
 				size = i + 1;
 				return;
@@ -82,13 +84,50 @@ public:
 
 	}
 };
+class Solution3 {
+public:
+	string fractionToDecimal(int numerator, int denominator) {
 
+		string str = "";
+		if (numerator == 0)return "0";
+		if ((numerator < 0 )^ (denominator < 0)) {
+			str = "-";
+
+		}
+		long num = abs(numerator);
+		long den = abs(denominator);
+		long head = num / den;
+		long tail = num % den;
+		str += to_string(head);
+		if (tail == 0)return str;
+		str += ".";
+		unordered_map<long, int> m;
+		m[tail] = str.size();
+		while (tail) {
+
+			tail = tail * 10;
+			str += to_string(tail / denominator);
+			tail = tail % denominator;
+			if (m.find(tail) == m.end()) {
+				m[tail] = str.size();
+			}
+			else {
+				int index = m[tail];
+				string str1 = str.substr(0, index);
+				string str2 = str.substr(index, str.size() - index);
+				str = str1 + "(" + str2 + ")";
+				break;
+			}
+		}
+		return str;
+	}
+};
 
 int main() {
-	int numerator =1;
-	int denominator =333;
+	int numerator =-1;
+	int denominator =-2147483648;
 	
-	Solution2 mysolu;
+	Solution3 mysolu;
 	string ans = mysolu.fractionToDecimal(numerator, denominator);
 	return 0;
 }
