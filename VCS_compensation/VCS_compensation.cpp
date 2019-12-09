6da4a4a9f2d7a8e8fd9458755d182aa6f9d113b2
vogage <1309416226@qq.com>
2019-12-09 15:58:01 +0800

    12-9
    增加补偿后的情况

--- VCS_compensation/VCS_compensation.cpp
+++ VCS_compensation/VCS_compensation.cpp
@@ -17,19 +17,47 @@ void VCS::initial_AC_test_trajectory(float Astart, float Aend, float Cstart, flo
 			vector<float> ac;
 			ac.push_back(a);
 			ac.push_back(c);
-			AC_test.push_back(ac);
+			AC.push_back(ac);
 		}
 	}
 	return;
 }
 
 
+void VCS::calculate_the_compensation_trajectory() {
+
+	for (int i = 0; i < AC_compensation.size(); i++) {
+		float A = AC_compensation[i].origin_NC.A;
+		float C = AC_compensation[i].origin_NC.C;
+
+		vector<float> AC_need_compensation;
+		AC_need_compensation.push_back(A);
+		AC_need_compensation.push_back(C);
+
+		vector<vector<float>> AC_element(4,vector<float>());
+		vector<vector<float>> AC_element_error(4,vector<float>());
+		find_AC_brick(AC_need_compensation, AC_element, AC_element_error);
+
+		vector<float> xyz_error(3,0);
+		get_AC_test_error(AC_element, AC_element_error, AC_need_compensation, xyz_error);
+
+		AC_compensation[i].Err_x = xyz_error[0];
+		AC_compensation[i].Err_y = xyz_error[1];
+		AC_compensation[i].Err_z = xyz_error[2];
+
+		AC_compensation[i].new_NC.A = AC_compensation[i].origin_NC.A;
+		AC_compensation[i].new_NC.C = AC_compensation[i].origin_NC.C;
+		AC_compensation[i].new_NC.X = AC_compensation[i].origin_NC.X - xyz_error[0];
+		AC_compensation[i].new_NC.Y = AC_compensation[i].origin_NC.Y - xyz_error[1];
+		AC_compensation[i].new_NC.Z = AC_compensation[i].origin_NC.Z - xyz_error[2];
+		
+	}
+	return;
+}
 
 
 
-
-void VCS::find_AC_brick(Array& AC, Array& AC_error, vector<float>& AC_need_compensation,
-	Array& AC_element, Array& AC_element_error){
+void VCS::find_AC_brick( vector<float>& AC_need_compensation,Array& AC_element, Array& AC_element_error){
 
 	int n = AC.size();
 	for (int i = 1; i <=4; i++) {
@@ -85,21 +113,21 @@ void VCS::find_AC_brick(Array& AC, Array& AC_error, vector<float>& AC_need_compe
 }
 
 
-void VCS::get_AC_test_error(Array& AC_element, Array& AC_element_error, vector<float> &AC_need_compensation,
-	vector<float>& xyz_error) {
+void VCS::get_AC_test_error(Array& AC_element, Array& AC_element_error, vector<float> &AC_need_compensation,vector<float>& xyz_error) {
 	float mean_a = (AC_element[0][0] + AC_element[2][0]) / 2;
 	float mean_c = (AC_element[0][1] + AC_element[1][1]) / 2;
 	float m = (AC_element[2][0] - AC_element[0][0]) / 2;
 	float n = (AC_element[1][1] - AC_element[0][1]) / 2;
 	float t = (AC_need_compensation[1] - mean_c) / n;
 	float s = (AC_need_compensation[0] - mean_a) / m;
-
+	
 	for (int i = 0; i < 4; i++) {
 		float tj = (AC_element[i][1] - mean_c) / n;
 		float sj = (AC_element[i][0] - mean_c) / m;
 		float Nj = (1 + t * tj)*(1 + s * sj) / 4;
 		for (int j = 0; j < xyz_error.size(); j++) {
-			xyz_error[j] = xyz_error[j] + AC_element_error[i][j] * Nj;
+			xyz_error[j] =xyz_error[j] + AC_element_error[i][j] * Nj;
 		}
 	}
+
 }
