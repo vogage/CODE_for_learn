5b6b53b52e7674c4130a558cbcf9918d8f732f6a
vogage <1309416226@qq.com>
2019-12-14 23:18:14 +0800

    12-14
    这我就不明白了，为啥我在PC端的结果和Leetcode网页的结果不一样呢

--- LeetCode/211_Add and Search Word - Data structure design.cpp
+++ LeetCode/211_Add and Search Word - Data structure design.cpp
@@ -2,6 +2,7 @@
 #include<vector>
 #include<string>
 #include<unordered_map>
+#include<stack>
 
 using namespace std;
 
@@ -37,96 +38,52 @@ public:
 
 	/** Returns if the word is in the data structure. A word could contain the dot character '.' to represent any one letter. */
 	bool search(string word) {
-		return DFS(word, 0, root);
+		//stack<TrNode*> st;
 		
+		return DFS(word, 0,root);
 	}
 
-	bool DFS(string word, int i,TrNode *root) {
+	bool DFS(string word, int index,TrNode *root) {
+		//if (root->children[word[index]] == NULL)return false;
 		if (root == NULL)return false;
-		if (word.size() - 1 == i) {
-			if (word[i] == '.') {
-				return root->isWord;
+		bool flag = false;
+		for (auto p : root->children) {
+			flag = true;
+		}
+		if (flag == false)return false;
+		if (index == word.size() - 1) {
+			//return root->children[word[index]]->isWord;
+			if (word[index] == '.') {
+				//if (word.size() == 1) {
+					for (auto p : root->children) {
+						if (p.second == NULL)return false;
+						else return p.second->isWord;
+					}
+					return false;
+				//}				
 			}
 			else {
-				return root->children[word[i]]->isWord;
+				if(root->children[word[index]]==NULL) return false;
+				else return root->children[word[index]]->isWord;
 			}
 		}
-		if (word[i] == '.') {
+		if (word[index] == '.') {
 			for (auto p : root->children) {
-				if (DFS(word, i + 1, p.second))return true;
+				if (DFS(word, index + 1, p.second)) return true;
 			}
-			return false;
+			//return false;
 		}
 		else {
-			TrNode *p = root->children[word[i]];
-			return DFS(word, i + 1, p);
-		}
-		
-	}
-	
-};
-
-/**
- * Your WordDictionary object will be instantiated and called as such:
- * WordDictionary* obj = new WordDictionary();
- * obj->addWord(word);
- * bool param_2 = obj->search(word);
- */
-
-class TrieNode1 {
-public:
-	//TrieNode1* children[26];
-	unordered_map<char, TrieNode1*>children;
-	bool is_word;
-
-	//initial your data structure here
-	TrieNode1(bool b = false) {
-		//memset(children, 0, sizeof(children));
-		is_word = false;
-	}
-};
-
-class Trie3 {
-public:
-	TrieNode1 *root;
-	/** Initialize your data structure here. */
-	Trie3() {
-		root = new TrieNode1();
-	}
-
-	/** Inserts a word into the trie. */
-	void insert(string word) {
-		TrieNode1 *p = root;
-		for (int i = 0; i < word.size(); i++) {
-			if (p->children[word[i]] == NULL) {
-				p->children[word[i]] = new TrieNode1();
-
+			if (root->children[word[index]] == NULL)return false;
+			else {
+				return DFS(word, index + 1, root->children[word[index]]);
 			}
-			p = p->children[word[i]];
 		}
-		p->is_word = true;
+		return false;
 	}
 
-	/** Returns if the word is in the trie. */
-	bool search(string word) {
-		TrieNode1* p = root;
-		for (int i = 0; i < word.size(); i++) {
-			if (p->children[word[i]] == NULL)return false;
-			p = p->children[word[i]];
 
-		}
-		return p->is_word;
-	}
-
-	/** Returns if there is any word in the trie that starts with the given prefix. */
-	bool startsWith(string prefix) {
-		TrieNode1 *p = root;
-		for (int i = 0; i < prefix.size(); i++) {
-			if (p->children[prefix[i]] == NULL)return false;
-			p = p->children[prefix[i]];
-		}
-		return true;
-	}
+	
 };
 
 
@@ -157,17 +114,37 @@ int main() {
 	//[[], ["at"], ["and"], ["an"], ["add"],
 	//["a"], [".at"], ["bat"], [".at"], ["an."], 
 	//["a.d."], ["b."], ["a.d"], ["."]]
-	 obj->addWord("at");
-	 obj->addWord("and");
-	 obj->addWord("an");
-	 obj->addWord("add");
-	 bool res0=obj->search("a");
-	 bool res1 = obj->search(".at");
-	 obj->addWord("bat");
-	 bool res2 = obj->search(".at");
-	 bool res3 = obj->search("b.");
-	 bool res4 = obj->search("a.d");
-	 bool res5 = obj->search(".");
+	 //obj->addWord("at");
+	 //obj->addWord("and");
+	 //obj->addWord("an");
+	 //obj->addWord("add");
+	 //bool res0=obj->search("a");
+	 //bool res1 = obj->search(".at");
+	 //obj->addWord("bat");
+	 //bool res2 = obj->search(".at");
+	 //bool res3 = obj->search("an.");
+	 //bool res4 = obj->search("a.d.");
+	 //bool res5 = obj->search("b.");
+	 //bool res6 = obj->search("a.d");
+	 //bool res7 = obj->search(".");
+
+	/* bool res1 = obj->search("a");*/
+
+	//["WordDictionary", "addWord", "addWord", "search", "search", 
+	//"search", "search", "search", "search", "search", "search"]
+	//[[], ["a"], ["ab"], ["a"], ["a."], ["ab"], 
+	//[".a"], [".b"], ["ab."], ["."], [".."]]
+
+	obj->addWord("a");
+	obj->addWord("ab");
+	bool res1 = obj->search("a");
+	bool res2 = obj->search("a.");
+	bool res3 = obj->search("ab");
+	bool res4 = obj->search(".a");
+	bool res5 = obj->search(".b");
+	bool res6 = obj->search("ab.");
+	bool res7 = obj->search(".");
+	bool res8 = obj->search("..");
 
 	return 0;
 }
