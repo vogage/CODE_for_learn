ebe2c0d019f92422d971ff9bd58f3b784d645371
vogage <1309416226@qq.com>
2020-01-30 22:34:28 +0800

    1-30
    看了答案，关于KMP算法还是得再研究研究

--- LeetCode/214_Shortest Palindrome.cpp
+++ LeetCode/214_Shortest Palindrome.cpp
@@ -40,12 +40,103 @@ public:
 		return true;
 	}
 };
+
+//class Solution3 {
+//	public String shortestPalindrome(String s) {
+//		String temp = s + "#" + new StringBuilder(s).reverse().toString();
+//		int[] table = getTable(temp);
+//
+//		//get the maximum palin part in s starts from 0
+//		return new StringBuilder(s.substring(table[table.length - 1])).reverse().toString() + s;
+//	}
+//
+//	public int[] getTable(String s) {
+//		//get lookup table
+//		int[] table = new int[s.length()];
+//
+//		//pointer that points to matched char in prefix part
+//
+//		int index = 0;
+//		//skip index 0, we will not match a string with itself
+//		for (int i = 1; i < s.length(); i++) {
+//			if (s.charAt(index) == s.charAt(i)) {
+//				//we can extend match in prefix and postfix
+//				table[i] = table[i - 1] + 1;
+//				index++;
+//			}
+//			else {
+//				//match failed, we try to match a shorter substring
+//
+//				//by assigning index to table[i-1], we will shorten the match string length, and jump to the 
+//				//prefix part that we used to match postfix ended at i - 1
+//				index = table[i - 1];
+//
+//				while (index > 0 && s.charAt(index) != s.charAt(i)) {
+//					//we will try to shorten the match string length until we revert to the beginning of match (index 1)
+//					index = table[index - 1];
+//				}
+//
+//				//when we are here may either found a match char or we reach the boundary and still no luck
+//				//so we need check char match
+//				if (s.charAt(index) == s.charAt(i)) {
+//					//if match, then extend one char 
+//					index++;
+//				}
+//
+//				table[i] = index;
+//			}
+//
+//		}
+//
+//		return table;
+//	}
+//
+//
+//};
+
+class Solution3 {
+public:
+	string shortestPalindrome(string s) {
+		string s_rev = s;
+		reverse(s_rev.begin(), s_rev.end());
+		vector<int> table;
+		string input = s + "#" + s_rev;
+		table = KMP(input);
+		return  s_rev.substr(0,s_rev.size() - table.back())+s;
+
+	}
+	vector<int> KMP(string input) {
+		int index = 0;
+		vector<int> table(input.size(), 0);
+		for (int i = 1; i < input.size(); i++) {
+			if (input[i] == input[index]) {
+				table[i] = table[i - 1] + 1;
+				index++;
+			}
+			else {
+				index = table[i - 1];
+				while (index > 0 && input[index] != input[i]) {
+					index = table[index - 1];
+				}
+
+				if (input[index] == input[i]) {
+					index++;
+				}
+				table[i] = index;
+			}
+		}
+		return table;
+	}
+
+
+};
+
 int main() { 
-	Solution2 mysolu;
+	Solution3 mysolu;
 
 	string input = "aacecaaa";
 	string input2 = "abcd";
 	string input3 = "a";
-	string res = mysolu.shortestPalindrome(input3);
+	string res = mysolu.shortestPalindrome(input);
 	return 0;
 }
